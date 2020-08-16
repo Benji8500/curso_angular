7d2c5068680364e0a3b8cd510642506b13633057
Benji8500 <benji.8500@gmail.com>
2020-08-16 19:02:27 -0400

    First observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {from, fromEvent} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'curso-angular';
+
+
+ngOnInit(){
+  const arr =from([1,2,3,4,5,6]);//este array ya es un observable
+
+  arr.subscribe(s => console.log('item:',s));
+
+  const aux = fromEvent(document, 'mousemove');
+
+  aux.subscribe(s => console.log('event: ', s.clientX + "," + s.clientY ))
+
+
+
+
+
+
+
+
+}
+ 
+
+
+
+
 }
