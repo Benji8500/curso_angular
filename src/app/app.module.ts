643c76535393021a2afc9d2845bae5459e157bb4
benji.8500 <benji.8500@gmail.com>
2020-08-23 17:48:25 -0400

    children routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 import {AppRouting} from './app.routing';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 
 
 
@@ -34,6 +36,8 @@ import {AppRouting} from './app.routing';
     HomeComponent,
     AboutComponent,
     AdminComponent,
+    Home1Component,
+    Home2Component,
 
 
   ],
