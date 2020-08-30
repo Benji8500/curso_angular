78f2031a80af02aefc118f0158da0a18d5daaf2a
benji.8500 <benji.8500@gmail.com>
2020-08-30 17:52:03 -0400

    estructura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,81 +7,9 @@ import {from, fromEvent} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'curso-angular';
-  name = 'Benjamin';
-  sw = true;
-  people = ['Alejandro', 'David', 'Jean-Luc', 'Quentin', 'Ingmar'];
-  aux = 3;
-  lastName ='Soto';
-
-  auxColor = 'red';
-
-  auxClass = true;
-
-  numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  persons = [
-    {
-      name: 'Juan',
-      lastName: 'Soria',
-      age: '200',
-      enable: true
-    },
-    {
-      name: 'Maria',
-      lastName: 'Soria',
-      age: '200',
-      enable: false
-    },
-    {
-      name: 'Marco',
-      lastName: 'Soria',
-      age: '200',
-      enable: true
-    },
-    {
-      name: 'Andres',
-      lastName: 'Soria',
-      age: '200',
-      enable: false
-    }
-  ];
-
-  auxNumber = 2;
-  exponent = 2;
-
-ngOnInit(){
-  const arr = from([1, 2, 3, 4, 5, 6]); // este array ya es un observable
-
-  arr.subscribe(s => console.log('item:', s));
-
-  const aux = fromEvent(document, 'mousemove');
-
-  // aux.subscribe(s => console.log('event: ', s.clientX + ',' + s.clientY ));
-
-  console.log('funcion pura:', this.suma(2, 3));
-  console.log('funcion impura:', this.impura(2, 3));
 
 
+  ngOnInit(): void {
 
-
-
-
-}
-suma(a: number, b: number){
-  return a + b;
-}
-
-impura(a: number, b: number){
-    return a + b + Math.random();
-}
-
-
-  saveClickChild(event){
-    console.log('Event Child: ', event);
-  }
-
-  changeName() {
-    this.name = 'benji';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test.component';
 import {FormsModule} from '@angular/forms';
-import { Test2Component } from './test2/test2.component';
-import {PersonaModule} from './modules/persona/persona.module';
-import {ProductoModule} from './modules/producto/producto.module';
-import {AdminModule} from './modules/admin/admin.module';
-import {UserModule} from './modules/user/user.module';
-import { TestPipe } from './pipes/test.pipe';
-import { ExpoPipe } from './pipes/expo.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { NgColorDirective } from './directives/ng-color.directive';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
 import {AppRouting} from './app.routing';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
-import { Userr1Component } from './modules/user-r/userr1/userr1.component';
-import { Userr2Component } from './modules/user-r/userr2/userr2.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
@@ -32,32 +15,14 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    Test2Component,
-    TestPipe,
-    ExpoPipe,
-    PurePipe,
-    ImpurePipe,
-    NgColorDirective,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component,
-    Userr1Component,
-    Userr2Component,
+    AppComponent
 
 
   ],
   imports: [
     BrowserModule,
-      BrowserAnimationsModule,
+    BrowserAnimationsModule,
     FormsModule,
-    PersonaModule,
-    ProductoModule,
-    AdminModule,
-    UserModule,
       AppRouting,
       NgbModule,
       NoopAnimationsModule,
