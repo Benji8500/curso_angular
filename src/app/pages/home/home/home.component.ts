2afeeb2ec3189efb1796b866b277dfd1f17c26ed
benji.8500 <benji.8500@gmail.com>
2020-08-30 20:39:17 -0400

    cards

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'aaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Sushi'
+    },
+    {
+      description: 'aaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Garbo'
+    },
+    {
+      description: 'aaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Cheese'
+    },
+    {
+      description: 'aaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'Salteña'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
